2e52d5208dc54e58a702af21df63ab780498d243
mpetersen49 <mpetersen49@gmail.com>
2021-05-03 21:59:23 -0500

    Loaded map and updated css

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,19 @@
 console.log("logic.js loaded")
+
+var myMap = L.map("mapid", {
+    center: [40.7608, -111.8910],
+    zoom: 5
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+
+  
